9bdc286f13d6f6d387228fa6bc0b3d46c2d61cf2
Asir Adnan <106885381+AsirAdnan@users.noreply.github.com>
2022-08-02 22:36:37 +0600

    Create 1012.py

---
+++ 1012.py
@@ -0,0 +1,6 @@
+A,B,C=[float(i) for i in input().split()]
+print('TRIANGULO: {:.3f}'.format(.5*A*C))
+print('CIRCULO: {:.3f}'.format(3.14159*C*C))
+print('TRAPEZIO: {:.3f}'.format(.5*(A+B)*C))
+print('QUADRADO: {:.3f}'.format(B**2))
+print('RETANGULO: {:.3f}'.format(B*A))
