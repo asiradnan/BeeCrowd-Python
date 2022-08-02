6540f30f3d60fc77ba31c17d489b424211810239
Asir Adnan <106885381+AsirAdnan@users.noreply.github.com>
2022-08-02 22:33:43 +0600

    Update 1010.py

--- 1010.py
+++ 1010.py
@@ -1,3 +1,3 @@
 a=input().split()
 b=input().split()
-print('VALOR A PAGAR: R$ {:.2f}'.format((float(a[1])*float([2]))+(float([1])*float([2]))))
+print('VALOR A PAGAR: R$ {:.2f}'.format((float(a[1])*float(a[2]))+(float(b[1])*float(b[2]))))
