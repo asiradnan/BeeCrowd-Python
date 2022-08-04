f53a78bf1a7dc28ab23f36aeb440bf7cb754648f
Asir Adnan <106885381+AsirAdnan@users.noreply.github.com>
2022-08-04 08:17:44 +0600

    Create 1047.py

---
+++ 1047.py
@@ -0,0 +1,24 @@
+A,B,C,D=[int(i for i in input().split())]
+if A==C:
+    if B==D:
+        print('O JOGO DUROU 24 HORA(S) E 0 MINUTO(S)')
+    elif D>B:
+        print(f'O JOGO DUROU 0 HORA(S) E {D-B} MINUTO(S)')
+    else:
+        print(f'O JOGO DUROU 23 HORA(S) E {60-B+D} MINUTO(S)')
+elif C>A:
+    E=C-A
+    if D>B:
+        print(f'O JOGO DUROU {E} HORA(S) E {D-B} MINUTO(S)')
+    elif D<B:
+        print(f'O JOGO DUROU {E-1} HORA(S) E {60-B+D} MINUTO(S)')
+    else:
+        print(f'O JOGO DUROU {E} HORA(S) E 0 MINUTO(S)')
+else:
+    E=24-A+C
+    if D>B:
+        print(f'O JOGO DUROU {E} HORA(S) E {D-B} MINUTO(S)')
+    elif D<B:
+        print(f'O JOGO DUROU {E-1} HORA(S) E {60-B+D} MINUTO(S)')
+    else:
+        print(f'O JOGO DUROU {E} HORA(S) E 0 MINUTO(S)') 
