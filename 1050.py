111181bd698574ca21c0d2efeb54a396252511ea
Asir Adnan <106885381+AsirAdnan@users.noreply.github.com>
2022-08-04 08:20:18 +0600

    Create 1050.py

---
+++ 1050.py
@@ -0,0 +1,19 @@
+odd=int(input())
+if odd==61:
+    print('Brasilia')
+elif odd==71:
+    print('Salvador')
+elif odd==11:
+    print('Sao Paulo')
+elif odd==21:
+    print('Rio de Janeiro')
+elif odd==32:
+    print('Juiz de Fora')
+elif odd==19:
+    print('Campinas')
+elif odd==27:
+    print('Vitoria')
+elif odd==31:
+    print('Belo Horizonte')
+else:
+    print('DDD nao cadastrado')
