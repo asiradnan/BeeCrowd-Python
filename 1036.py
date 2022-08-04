9b7c192f33cd639e3ddc1113dd2516dd95df2418
Asir Adnan <106885381+AsirAdnan@users.noreply.github.com>
2022-08-04 08:00:32 +0600

    Create 1036.py

---
+++ 1036.py
@@ -0,0 +1,7 @@
+A,B,C,D=[float(i) for i in input().split()]
+D=(B**2)-(4*A*C)
+if D>=0 and A!=0:
+    print('R1 = {:.5f}'.format((-B+(D**.5))/(2*A)))
+    print('R2 = {:.5f}'.format((-B-(D**.5))/(2*A))) 
+else:
+    print('Impossivel calcular')
