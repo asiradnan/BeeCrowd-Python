8c3dcd3503faf1cc3f6ef9d4776a657b4163189b
Asir Adnan <106885381+AsirAdnan@users.noreply.github.com>
2022-08-05 23:46:29 +0600

    Update 1117.py

--- 1117.py
+++ 1117.py
@@ -1,7 +1,10 @@
-N=int(input())
-for i in range(N):
-    X,Y=[int(i) for i in input().split()]
-    if Y==0:
-        print('divisao impossivel')
+list1=[]
+while True:
+    a=float(input())
+    if 0<=a<=10:
+        list1.append(a)
     else:
-        print('{:.1f}'.format(X/Y))
+        print('nota invalida')
+    if len(list1)==2:
+        print('media = {:.2f}'.format((list1[0]+list1[1])/2))
+        break
