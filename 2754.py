38cd8fa3f69b417b7c1625e6c43c0493f8b221d6
Asir Adnan <106885381+AsirAdnan@users.noreply.github.com>
2022-08-08 07:43:37 +0600

    Create 2754.py

---
+++ 2754.py
@@ -0,0 +1,11 @@
+a=234.345
+b=45.698
+print('{0:.6f} - {1:.6f}'.format(a,b))
+print('{0:.0f} - {1:.0f}'.format(a,b))
+print('{0:.1f} - {1:.1f}'.format(a,b))
+print('{0:.2f} - {1:.2f}'.format(a,b))
+print('{0:.3f} - {1:.3f}'.format(a,b))
+print('{0:e} - {1:e}'.format(a,b))
+print('{0:E} - {1:E}'.format(a,b))
+print('{0:.3f} - {1:.3f}'.format(a,b))
+print('{0:.3f} - {1:.3f}'.format(a,b))
