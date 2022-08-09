dbe26cb5dab62d510a73d52364cc4d2d4b75c49d
Asir Adnan <106885381+AsirAdnan@users.noreply.github.com>
2022-08-09 21:17:46 +0600

    Update 2846.py

--- 2846.py
+++ 2846.py
@@ -1,15 +1,11 @@
-while True:
-    try:
-        a=2
-        b=3
-        fibonot=[]
-        i=int(input())
-        while len(fibonot)<i:
-            c=a+b
-            for j in range(b+1,c):
-                fibonot.append(j)
-            a=b
-            b=c
-        print(fibonot[i-1])
-    except:
-        break
+a=2
+b=3
+fibonot=[]
+i=int(input())
+while len(fibonot)<i:
+    c=a+b
+    for j in range(b+1,c):
+        fibonot.append(j)
+    a=b
+    b=c
+print(fibonot[i-1])
